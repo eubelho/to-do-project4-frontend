c30fdcd2090a0f9e1cf2ffed2b57ff1aff0affb7
Elliott Ubelhor <elliottubelhor@MacBook-Air.local>
2023-11-07 15:37:56 -0600

    final commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
 
   refreshList = () => {
     axios
-      .get("/api/todos/")
+      .get("http://localhost:8000/api/todos/")
       .then((res) => this.setState({ todoList: res.data }))
       .catch((err) => console.log(err));
   };
@@ -37,18 +37,18 @@ class App extends Component {
 
     if (item.id) {
       axios
-        .put(`/api/todos/${item.id}/`, item)
+        .put(`http://localhost:8000/api/todos/${item.id}/`, item)
         .then((res) => this.refreshList());
       return;
     }
     axios
-      .post("/api/todos/", item)
+      .post("http://localhost:8000/api/todos/", item)
       .then((res) => this.refreshList());
   };
 
   handleDelete = (item) => {
     axios
-      .delete(`/api/todos/${item.id}/`)
+      .delete(`http://localhost:8000/api/todos/${item.id}/`)
       .then((res) => this.refreshList());
   };
 
